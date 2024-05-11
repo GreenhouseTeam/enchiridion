48930c1e8d3408684691016aa12a1dad0c92d36a
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-05-11 16:29:06 +1000

    Fix values for particles again.

--- common/src/main/java/dev/greenhouseteam/enchiridion/mixin/client/LivingEntityMixin.java
+++ common/src/main/java/dev/greenhouseteam/enchiridion/mixin/client/LivingEntityMixin.java
@@ -26,9 +26,9 @@ public abstract class LivingEntityMixin extends Entity {
                 return;
             if (Enchiridion.getHelper().isFrozenByEnchantment(this) && !isInPowderSnow && tickCount % 8 == 0 && getTicksFrozen() > getTicksRequiredToFreeze())
                 for (int i = 0; i < 4; ++i) {
-                    double x = getX() + (getBoundingBox().getXsize() * random.nextDouble());
+                    double x = getX() + (getBoundingBox().getXsize() / 2 * random.nextDouble());
                     double y = getY(random.nextDouble());
-                    double z = getZ() + (getBoundingBox().getZsize() * random.nextDouble());
+                    double z = getZ() + (getBoundingBox().getZsize() / 2 * random.nextDouble());
                     level().addParticle(ParticleTypes.SNOWFLAKE, x, y, z, random.nextDouble() * 0.025, random.nextDouble() * 0.025, random.nextDouble() * 0.025);
                 }
         }
